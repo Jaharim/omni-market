a17c5e69c09669465c665b027653fe8f5f7f0b04
SeungPyo Hong <83650872+Jaharim@users.noreply.github.com>
2024-05-28 22:05:26 +0900

    fix: 환경변수 사용 방식 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.API_URL': JSON.stringify(env.API_URL),
-    },
-    plugins: [react()],
-    base: '/omni-market/', // 이 부분을 수정
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/omni-market/', // 이 부분을 수정
 });
