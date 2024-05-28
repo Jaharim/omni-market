a697966872911a6b450fa6874c233b34c405589e
SeungPyo Hong <83650872+Jaharim@users.noreply.github.com>
2024-05-28 21:08:19 +0900

    feat: 페이지 추가에 따른 라우터 설정 변경
    - 장바구니, 회원가입, 마이페이지, 판매자센터, 판매자센터 상품 추가 및 수정, 검색결과, 주문결제 페이지 추가
    - 라우터 에러 발생 시 출력 페이지 추가

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,23 +3,70 @@ import App from '../App';
 import Home from '../pages/Home';
 import ProductDetail from '../pages/ProductDetail';
 import Login from '../pages/Login';
+import Cart from '../pages/Cart';
+import SignUp from '../pages/SignUp';
+import SearchedProducts from '../pages/SearchedProducts';
+import SellerCenter from '../pages/SellerCenter';
+import SellerEditProduct from '../pages/SellerEditProduct';
+import OrderPage from '../pages/OrderPage';
+import MyPage from '../pages/MyPage';
+import ErrorPage from '../pages/ErrorPage';
 
 const AppRouter = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '',
         element: <Home />,
+        errorElement: <ErrorPage />,
       },
       {
         path: '/products/:productId/',
         element: <ProductDetail />,
+        errorElement: <ErrorPage />,
       },
       {
         path: '/login',
         element: <Login />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/myPage',
+        element: <MyPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/sellerCenter',
+        element: <SellerCenter />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/sellerCenter/editProduct',
+        element: <SellerEditProduct />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/products',
+        element: <SearchedProducts />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/order',
+        element: <OrderPage />,
+        errorElement: <ErrorPage />,
       },
     ],
   },
